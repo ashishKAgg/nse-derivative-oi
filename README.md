efd0d9de05ad75356fb734fc73f2f133174158f7
Ashish <aashish.2k8@gmail.com>
2026-04-11 08:23:25 +0530

    NSE option data initial changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nse-derivative-oi
-Web scrapper for NSE derivatives data
+Web scrapper for NSE derivatives data. GHA workflow runs after every 5 minutes to capture the change in Options OI data for the list of index provided in the workflow matrix.
+It process the data of current and upcoming Option data expiry and save it in to file named with `Expiry date`. If file already exists then it appends the data into file, otherwise creates a new file and push the changes to repository.
+
+## Steps to add more indexes:
+Currently, it supports only Nifty50. To add more indexes:
+- Open `nse_option_scheduler.yaml` workflow file.
+- Add new object with `symbol` and `name` as matrix item on line 17.
---
+++ nse_scraper.py
@@ -0,0 +1,129 @@
+import requests
+import os, time
+import pandas as pd
+import json, warnings
+warnings.filterwarnings("ignore")
+from datetime import datetime
+
+indexSymbol = os.getenv('index_symbol', 'nse50_opt')
+indexName = os.getenv('index_name', 'nifty50')
+
+nseUrl = "https://www.nseindia.com/market-data/equity-derivatives-watch"
+nseOptionsUrl = f"https://www.nseindia.com/api/liveEquity-derivatives?index={indexSymbol}"
+
+headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36", 
+          "referer": "https://www.nseindia.com/market-data/equity-derivatives-watch"}
+
+outputDir = "C:\\xyz\\optionChart\\nifty\\optionsOI\\"
+prefixPath = outputDir + "optionsoi-"
+
+repoDirectory = f'optionOIData/{indexName}'
+repoPrefixPath = f'{repoDirectory}/{indexName}-'
+
+def createDirIfNotExists(csvPath):
+    dirPath = csvPath[:csvPath.rfind("\\")]
+    if not os.path.exists(dirPath):
+        os.makedirs(dirPath)
+
+
+def fetchOptionsFromNSE():
+    #Handle Cookies
+    with requests.Session() as sess:
+        sess.get(nseUrl, headers=headers, verify=False)
+        resp = sess.get(nseOptionsUrl, headers=headers, verify=False)
+
+    if resp.status_code == 200:
+        data = json.loads(resp.text)
+        #Transform json to DataFrame
+        df = pd.DataFrame(data['data'])
+        #Added DateTime column
+        df["datetime"] = datetime.now()
+        #Remove unused columns
+        removeUnusedFeatures(df)
+        #Select only current and next week data
+        #print(df.head())
+        df = selectTwoWeeksData(df)
+    else:
+        print("Error Occurred..Status Code:"+str(resp.status_code))
+    return df
+
+
+def removeUnusedFeatures(df):
+    del df["underlying"]
+    del df["identifier"] 
+    del df["instrumentType"] 
+    del df["instrument"]
+    return df
+
+
+def selectTwoWeeksData(df):
+    currWeek = int(datetime.now().isocalendar()[1])
+    df["expiryDate_week"] = pd.to_datetime(df["expiryDate"], format="%d-%b-%Y").dt.isocalendar().week
+    ndf = df[(df["lastPrice"] > 0) & ((df["expiryDate_week"] == currWeek) | (df["expiryDate_week"] == currWeek+1))]
+    del ndf["expiryDate_week"]
+    ndf.reset_index(inplace=True, drop=True)
+    return ndf
+
+
+def saveToFile(df, prefixPath):
+    grouped = df.groupby("expiryDate")
+    for name, group in grouped:
+        csvPath = prefixPath + name + ".csv"
+        #TODO: find file with latest counter and generate new CSVPath
+        #csvPath = checkAndCreateNew(csvPath)
+        if os.path.exists(csvPath):
+            group.to_csv(csvPath, mode="a", index=False, header=False)
+        else:
+            group.to_csv(csvPath, mode="a", index=False)
+
+
+def saveToRepo(df, directory, repoPrefixPath):
+    # Create the folder if it doesn't exist (equivalent to 'mkdir -p')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print(f"Created directory: {directory}")
+
+    grouped = df.groupby("expiryDate")
+    for name, group in grouped:
+        csvPath = repoPrefixPath + name + ".csv"
+        if os.path.exists(csvPath):
+            group.to_csv(csvPath, mode="a", index=False, header=False)
+        else:
+            group.to_csv(csvPath, mode="a", index=False)
+
+
+def checkAndCreateNew(csvPath):
+    newCsvPath = csvPath
+    
+    if os.path.exists(csvPath):
+        maxFileSize = 1000
+        currFileSizeInKB = os.stat(csvPath).st_size/1000
+        delimiter = '_'
+        
+        if currFileSizeInKB > maxFileSize:
+            nameExtSplit = os.path.splitext(csvPath)
+            filename = nameExtSplit[0]
+            lastOcc = filename.rfind(delimiter)
+            if lastOcc == -1:
+                counter = 1
+                lastOcc = len(filename)
+            else:
+                counter = int(filename[lastOcc + 1:]) + 1
+
+            newCsvPath = filename[:lastOcc] + delimiter + str(counter) + nameExtSplit[1]
+            print("new file created:"+newCsvPath)
+    return newCsvPath
+
+
+if __name__ == "__main__":
+    # timer = 180
+    while True:
+        df = fetchOptionsFromNSE()
+        # saveToFile(df,prefixPath)
+        saveToRepo(df, repoDirectory, repoPrefixPath)
+        # Scheduling will be controlled by the workflow itself
+        # if((datetime.now().hour > 14) & (datetime.now().minute > 30)):
+        #     break
+        # else:
+        #     #break
+        #     time.sleep(timer)
